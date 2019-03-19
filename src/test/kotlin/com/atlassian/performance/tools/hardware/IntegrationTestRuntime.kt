dfdb418a5bdf085655c4a4f0c7a5b12dbd7f4cfa
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-03-19 17:40:54 +0100

    Stitch results from `QUICK-94-Large-Result.zip`

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 object IntegrationTestRuntime {
 
-    const val taskName = "QUICK-94-run-on-ci"
+    const val taskName = "QUICK-94-stitched"
     val workspace = RootWorkspace(Paths.get("build")).isolateTask(taskName)
 
     val aws: Aws
