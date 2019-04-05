a0e7fe92a1636eae8149c2700482c606dd843e23
Kevin Ma <kma2@atlassian.com>
2019-04-05 11:39:49 +1100

    Updated task name

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 object IntegrationTestRuntime {
 
-    const val taskName = "QUICK-110-3rd-go"
+    const val taskName = "QUICK-103"
     val workspace = RootWorkspace(Paths.get("build")).isolateTask(taskName)
 
     val aws: Aws
