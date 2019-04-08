59157304a71f3ccd7e50bacb5cf9a9d48b12321b
Kevin Ma <kma2@atlassian.com>
2019-04-08 10:24:59 +1000

    Changed password for CustomeSetup

--- virtual-users/src/main/kotlin/com/atlassian/performance/tools/hardware/vu/CustomSetup.kt
+++ virtual-users/src/main/kotlin/com/atlassian/performance/tools/hardware/vu/CustomSetup.kt
@@ -22,7 +22,7 @@ class CustomSetup(
     private val logger = LogManager.getLogger(this::class.java)
 
     private val driver: WebDriver = jira.driver
-    private val access: AdminAccess = AdminAccess(driver, jira, "admin")
+    private val access: AdminAccess = AdminAccess(driver, jira, jira.adminPassword())
     private val switchLocator = By.id("rte-switch")
 
     override fun run() {
