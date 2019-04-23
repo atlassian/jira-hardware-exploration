a57094301c6d2a2f594736eab3213c1c75ca2f58
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-04-23 17:36:07 +0200

    QUICK-125: Discard old red-build results

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 object IntegrationTestRuntime {
 
-    const val taskName = "QUICK-125"
+    const val taskName = "QUICK-125-v2"
     val workspace = RootWorkspace(Paths.get("build")).isolateTask(taskName)
 
     val aws: Aws
