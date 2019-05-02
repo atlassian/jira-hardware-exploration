ba8d723933c1cdfd266216e6896421eb83b1736e
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-05-02 19:25:30 +0200

    QUICK-132: Have more patience for "Browse Boards"

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 object IntegrationTestRuntime {
 
-    const val taskName = "QUICK-124-cherry-picked-v2"
+    const val taskName = "QUICK-132-fix-v0"
     val workspace = RootWorkspace(Paths.get("build")).isolateTask(taskName)
 
     val aws: Aws
