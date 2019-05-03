d2667e465b2218854ac125b6770b51c927974410
Kevin Ma <kma2@atlassian.com>
2019-05-03 10:13:57 +1000

    Revert "Added more write actions"
    This reverts commit 474c66ff2f7be8054657325e008e44614b15ba13.

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 object IntegrationTestRuntime {
 
-    const val taskName = "QUICK-124-jira7-xl-Mysql-More-WriteAction-2nd"
+    const val taskName = "QUICK-124-jira7-xl-Mysql-More-WriteAction"
     val workspace = RootWorkspace(Paths.get("build")).isolateTask(taskName)
 
     val aws: Aws
