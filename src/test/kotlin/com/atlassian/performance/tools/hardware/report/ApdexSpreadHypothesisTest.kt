d58036776379d2cc7144ce6b5cb42796a7f82d39
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-05-24 13:19:36 +0200

    QUICK-123: Test H1 with thresholds 0.03/0.03

---
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/report/ApdexSpreadHypothesisTest.kt
@@ -0,0 +1,71 @@
+package com.atlassian.performance.tools.hardware.report
+
+import com.atlassian.performance.tools.hardware.HardwareExplorationResult
+import com.atlassian.performance.tools.hardware.HardwareExplorationResultCache
+import com.atlassian.performance.tools.hardware.HardwareTestResult
+import org.junit.Test
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class ApdexSpreadHypothesisTest {
+
+
+    /**
+     * H1 large error rate spread => large apdex spread
+     */
+    @Test
+    fun highErrorRateSpreadShouldCauseHighApdexSpread() {
+        val processedCache = Paths.get("build/jpt-workspace/QUICK-132-fix-v3/processed-cache.json")
+        val exploration = readExploration(processedCache)
+        val testResults = exploration.mapNotNull { it.testResult }
+        val resultsPerApdexSpread = testResults.groupBy { it.apdexSpread() > 0.03 }
+        val highApdexSpread: List<HardwareTestResult> = resultsPerApdexSpread[true]
+            ?: error("No results for high Apdex spread")
+        val resultsPerErrorRateSpread = testResults.groupBy { it.errorRateSpread() > 0.03 }
+        val highErrorRateSpread: List<HardwareTestResult> = resultsPerErrorRateSpread[true]
+            ?: error("No results for high error rate spread")
+
+        val quality = measureQuality(
+            population = testResults,
+            sick = highApdexSpread,
+            diagnosed = highErrorRateSpread
+        )
+
+        println("quality = $quality")
+    }
+
+    private fun measureQuality(
+        population: List<*>,
+        sick: List<*>,
+        diagnosed: List<*>
+    ): TestQuality {
+        val healthy = population - sick
+        val undiagnosed = population - diagnosed
+
+        val truePositives = sick.intersect(diagnosed)
+        val trueNegatives = healthy.intersect(undiagnosed)
+        val falsePositives = healthy.intersect(diagnosed)
+        val falseNegatives = sick.intersect(undiagnosed)
+
+        val sensitivity = truePositives.size.toDouble() / sick.size.toDouble()
+        val specificity = trueNegatives.size.toDouble() / healthy.size.toDouble()
+        return TestQuality(sensitivity, specificity)
+    }
+
+    private data class TestQuality(
+        val sensitivity: Double,
+        val specificity: Double
+    )
+
+    private fun readExploration(
+        resourcePath: Path
+    ): List<HardwareExplorationResult> = HardwareExplorationResultCache(resourcePath).read()
+
+    private fun HardwareTestResult.apdexSpread(): Double {
+        return apdexes.max()!! - apdexes.min()!!
+    }
+
+    private fun HardwareTestResult.errorRateSpread(): Double {
+        return errorRates.max()!! - errorRates.min()!!
+    }
+}
