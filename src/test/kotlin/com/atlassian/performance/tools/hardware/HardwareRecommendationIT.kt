b7b073eebafec1dc21039397fb8bee607a8c84f0
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-06-12 15:46:49 +0200

    QUICK-174: Fix logging

--- src/test/kotlin/com/atlassian/performance/tools/hardware/HardwareRecommendationIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/HardwareRecommendationIT.kt
@@ -3,26 +3,33 @@ package com.atlassian.performance.tools.hardware
 import com.amazonaws.services.ec2.model.InstanceType
 import com.amazonaws.services.ec2.model.InstanceType.*
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder
-import com.atlassian.performance.tools.hardware.IntegrationTestRuntime.aws
-import com.atlassian.performance.tools.hardware.IntegrationTestRuntime.taskName
-import com.atlassian.performance.tools.hardware.IntegrationTestRuntime.workspace
 import com.atlassian.performance.tools.hardware.guidance.JiraExplorationGuidance
 import com.atlassian.performance.tools.hardware.tuning.HeapTuning
 import com.atlassian.performance.tools.hardware.tuning.JiraNodeTuning
 import com.atlassian.performance.tools.hardware.tuning.NoTuning
 import com.atlassian.performance.tools.infrastructure.api.distribution.PublicJiraSoftwareDistribution
+import com.atlassian.performance.tools.lib.LogConfigurationFactory
 import com.atlassian.performance.tools.lib.s3cache.S3Cache
 import com.atlassian.performance.tools.lib.workspace.GitRepo2
 import com.atlassian.performance.tools.virtualusers.api.TemporalRate
 import com.atlassian.performance.tools.workspace.api.TaskWorkspace
+import org.apache.logging.log4j.core.config.ConfigurationFactory
 import org.eclipse.jgit.api.Git
+import org.junit.Before
 import org.junit.Test
 import java.io.File
 import java.time.Duration
 
 class HardwareRecommendationIT {
 
+    private val taskName = "QUICK-132-fix-v3"
+    private val workspace = IntegrationTestRuntime.rootWorkspace.isolateTask(taskName)
     private val jswVersion = System.getProperty("hwr.jsw.version") ?: "7.13.0"
+
+    @Before
+    fun setUp() {
+        ConfigurationFactory.setConfigurationFactory(LogConfigurationFactory(workspace))
+    }
 
     @Test
     fun shouldRecommendHardwareForExtraLarge() {
@@ -48,6 +55,7 @@ class HardwareRecommendationIT {
         db: InstanceType
     ): List<Recommendation> {
         requireCleanRepo()
+        val aws = IntegrationTestRuntime.prepareAws()
         val taskWorkspace = TaskWorkspace(workspace.directory.resolve(scale.description))
         val engine = HardwareRecommendationEngine(
             product = PublicJiraSoftwareDistribution(jswVersion),
@@ -76,6 +84,7 @@ class HardwareRecommendationIT {
             maxErrorRate = 0.01,
             minApdex = 0.70,
             repeats = 2,
+            aws = aws,
             workspace = taskWorkspace,
             s3Cache = S3Cache(
                 transfer = TransferManagerBuilder.standard()
