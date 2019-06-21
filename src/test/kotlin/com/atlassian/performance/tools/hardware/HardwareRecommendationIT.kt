a40e2a4883c6a557b8d1e2c1bce0dc14a46bb6e9
Damian Kedzierski <dkedzierski@atlassian.com>
2019-06-21 14:19:07 +0200

    Test load balancer

--- src/test/kotlin/com/atlassian/performance/tools/hardware/HardwareRecommendationIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/HardwareRecommendationIT.kt
@@ -17,7 +17,7 @@ import java.time.Duration
 
 class HardwareRecommendationIT {
 
-    private val taskName = "DCNG-195-7nodes"
+    private val taskName = "DCNG-195-7nodes-lb"
     private val workspace = IntegrationTestRuntime.rootWorkspace.isolateTask(taskName)
     private val jswVersion = System.getProperty("hwr.jsw.version") ?: "8.1.0"
 
