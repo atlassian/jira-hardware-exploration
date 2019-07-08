e93bf82e5609bf314cd61960935a2e892997dc88
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-07-08 17:45:56 +0200

    QUICK-213: Nuke dataset apps on the fly

--- src/test/kotlin/com/atlassian/performance/tools/lib/DatabaseOverride.kt
+++ src/test/kotlin/com/atlassian/performance/tools/lib/DatabaseOverride.kt
@@ -2,11 +2,20 @@ package com.atlassian.performance.tools.lib
 
 import com.atlassian.performance.tools.infrastructure.api.database.Database
 import com.atlassian.performance.tools.infrastructure.api.dataset.Dataset
+import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomeSource
 
 fun Dataset.overrideDatabase(
     override: (Dataset) -> Database
 ): Dataset = Dataset(
     database = override(this),
     jiraHomeSource = jiraHomeSource,
+    label = label
+)
+
+fun Dataset.overrideJiraHome(
+    override: (Dataset) -> JiraHomeSource
+): Dataset = Dataset(
+    database = database,
+    jiraHomeSource = override(this),
     label = label
 )
---
+++ src/test/kotlin/com/atlassian/performance/tools/lib/infrastructure/AppNukingJiraHome.kt
@@ -0,0 +1,15 @@
+package com.atlassian.performance.tools.lib.infrastructure
+
+import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomeSource
+import com.atlassian.performance.tools.ssh.api.SshConnection
+
+class AppNukingJiraHome(
+    private val delegate: JiraHomeSource
+) : JiraHomeSource {
+
+    override fun download(ssh: SshConnection): String {
+        val remotePath = delegate.download(ssh)
+        ssh.execute("rm -rf $remotePath/plugins")
+        return remotePath
+    }
+}
