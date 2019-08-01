8a90e02618adf8293ff8e2607345c42393f16d0e
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2019-08-01 17:06:30 +0200

    QUICK-240: Stop requiring a license in `HardwareExplorationTableTest`

--- src/test/kotlin/com/atlassian/performance/tools/hardware/report/HardwareExplorationTableTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/report/HardwareExplorationTableTest.kt
@@ -2,18 +2,37 @@ package com.atlassian.performance.tools.hardware.report
 
 import com.amazonaws.services.ec2.model.InstanceType
 import com.atlassian.performance.tools.hardware.*
+import com.atlassian.performance.tools.infrastructure.api.database.Database
+import com.atlassian.performance.tools.infrastructure.api.database.DbType
+import com.atlassian.performance.tools.infrastructure.api.dataset.Dataset
+import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomeSource
 import com.atlassian.performance.tools.lib.Ratio
+import com.atlassian.performance.tools.lib.infrastructure.AdminDataset
 import com.atlassian.performance.tools.lib.report.VirtualUsersPresenceJudge
 import com.atlassian.performance.tools.report.api.result.RawCohortResult
+import com.atlassian.performance.tools.ssh.api.SshConnection
+import com.atlassian.performance.tools.virtualusers.api.TemporalRate
+import com.atlassian.performance.tools.virtualusers.api.VirtualUserLoad
 import com.atlassian.performance.tools.workspace.api.RootWorkspace
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.io.File
+import java.net.URI
 import java.nio.file.Paths
+import java.time.Duration
 
 class HardwareExplorationTableTest {
 
     private val workspace = RootWorkspace(Paths.get("build")).currentTask.isolateTest(javaClass.simpleName)
+    private val dummyDataset = AdminDataset(
+        dataset = Dataset(
+            FailingDatabase(),
+            FailingJiraHome(),
+            "dummy"
+        ),
+        adminLogin = "a",
+        adminPassword = "a"
+    )
 
     @Test
     fun shouldSummarize() {
@@ -29,7 +48,18 @@ class HardwareExplorationTableTest {
                 reason = "It's a unit test"
             ),
             testResult = HardwareMetric(
-                scale = ApplicationScales().large("7.5.0"),
+                scale = ApplicationScale(
+                    description = "JIRA-JPTC-1339",
+                    cacheKey = "JIRA-JPTC-1339",
+                    dataset = dummyDataset,
+                    load = VirtualUserLoad.Builder()
+                        .virtualUsers(75)
+                        .ramp(Duration.ofSeconds(90))
+                        .flat(Duration.ofMinutes(20))
+                        .maxOverallLoad(TemporalRate(15.0, Duration.ofSeconds(1)))
+                        .build(),
+                    vuNodes = 6
+                ),
                 presenceJudge = VirtualUsersPresenceJudge(Ratio(0.12)), // partial data
                 errorRateWarningThreshold = 0.05
             ).score(
@@ -52,3 +82,16 @@ class HardwareExplorationTableTest {
         )
     }
 }
+
+class FailingDatabase : Database {
+
+    override fun getDbType(): DbType = fail()
+    override fun setup(ssh: SshConnection): String = fail()
+    override fun start(jira: URI, ssh: SshConnection): Unit = fail()
+}
+
+class FailingJiraHome : JiraHomeSource {
+    override fun download(ssh: SshConnection): String = fail()
+}
+
+private fun fail(): Nothing = throw Exception("unexpected call")
