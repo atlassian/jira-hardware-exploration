45f97758234ac217050a82f8f2174b3bfa57e3bb
Ewelina Fiedorowicz <efiedorowicz@atlassian.com>
2020-10-02 10:45:01 +0200

    MNSTR-4529 Add new provider to providers chain regarding to migration to IRSA

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -1,9 +1,6 @@
 package com.atlassian.performance.tools.hardware
 
-import com.amazonaws.auth.AWSCredentialsProviderChain
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper
-import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider
+import com.amazonaws.auth.*
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 import com.atlassian.performance.tools.aws.api.Aws
@@ -16,20 +13,26 @@ import java.util.*
 object IntegrationTestRuntime {
 
     val rootWorkspace = RootWorkspace(Paths.get("build"))
+    private const val roleArn: String = "arn:aws:iam::695067801333:role/server-gdn-bamboo"
+    private val region = Regions.EU_WEST_1
 
     fun prepareAws() = Aws(
         credentialsProvider = AWSCredentialsProviderChain(
             STSAssumeRoleSessionCredentialsProvider.Builder(
-                "arn:aws:iam::695067801333:role/server-gdn-bamboo",
+                roleArn,
                 UUID.randomUUID().toString()
             ).build(),
             ProfileCredentialsProvider("jpt-dev"),
             EC2ContainerCredentialsProviderWrapper(),
+            WebIdentityTokenCredentialsProvider.builder()
+                .roleArn(roleArn)
+                .roleSessionName(UUID.randomUUID().toString())
+                .build(),
             DefaultAWSCredentialsProviderChain()
         ),
-        region = Regions.EU_WEST_1,
+        region = region,
         regionsWithHousekeeping = listOf(Regions.EU_WEST_1),
-        capacity = TextCapacityMediator(Regions.EU_WEST_1),
+        capacity = TextCapacityMediator(region),
         batchingCloudformationRefreshPeriod = Duration.ofSeconds(20)
     )
 }
